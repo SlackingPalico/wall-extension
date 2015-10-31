a1355d129ecb9722410d066f7cd34a41f3491dd3
SlackingPalico <joeldado08@gmail.com>
2015-10-31 14:25:18 -0700

    [feat] filled up manifest.json with permissions and backgroundand user will go to google homepage on click event

--- Extension Files/src/background.js
+++ Extension Files/src/background.js
@@ -1,3 +1,10 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+var googleURL = 'https://www.google.com/?gws_rd=ssl',
+googleURL2 = 'https://www.google.com/';
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+  if (tab.url !== googleURL || tab.url !== googleURL2){
+    chrome.tabs.update(tab.id, {
+        url: googleURL
+    });
+  }
 });
